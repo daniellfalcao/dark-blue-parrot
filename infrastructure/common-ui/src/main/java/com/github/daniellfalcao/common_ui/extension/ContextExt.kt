8a80d894c9749053d6b7fd6fdaa0c55f975e3ec0
DanielFalcao <dev.danielfalcao@gmail.com>
2021-07-07 19:05:28 -0300

    Creating PreSignFragment and sign in/sign up fragment skeleton

---
+++ infrastructure/common-ui/src/main/java/com/github/daniellfalcao/common_ui/extension/ContextExt.kt
@@ -0,0 +1,18 @@
+package com.github.daniellfalcao.common_ui.extension
+
+import android.content.Context
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+
+/**
+ * Wrap ContextCompat.getDrawable call into a extension of Context.
+ *
+ * */
+fun Context.compatDrawable(@DrawableRes id: Int) = ContextCompat.getDrawable(this, id)
+
+/**
+ * Wrap ContextCompat.getColor call into a extension of Context.
+ *
+ * */
+fun Context.compatColor(@ColorRes id: Int) = ContextCompat.getColor(this, id)
