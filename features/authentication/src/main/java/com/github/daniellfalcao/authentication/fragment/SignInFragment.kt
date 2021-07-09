0e3c7851dfb8dfc8235bc35ab087c891a496cd0d
DanielFalcao <dev.danielfalcao@gmail.com>
2021-07-09 01:32:10 -0300

    Handling data from sign up in sign in

--- features/authentication/src/main/java/com/github/daniellfalcao/authentication/fragment/SignInFragment.kt
+++ features/authentication/src/main/java/com/github/daniellfalcao/authentication/fragment/SignInFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.setupWithNavController
 import com.github.daniellfalcao.common_ui.ui.binding.withBinding
 import com.github.daniellfalcao.common_ui.ui.fragment.ParrotFragment
@@ -13,23 +14,21 @@ import com.github.daniellfalcao.darkblueparrot.authentication.databinding.Fragme
 
 class SignInFragment : ParrotFragment<FragmentSignInBinding>() {
 
-    override fun onCreateViewBinding(inflater: LayoutInflater, container: ViewGroup?) {
-        binding = FragmentSignInBinding.inflate(inflater, container, false)
-    }
+    private val args: SignInFragmentArgs by navArgs()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        withBinding {
-            val navController = findNavController()
+    override fun onCreateViewBinding(inflater: LayoutInflater, container: ViewGroup?) {
+        binding = binding ?: FragmentSignInBinding.inflate(inflater, container, false).apply {
             toolbar.apply {
-                setupWithNavController(navController)
+                setupWithNavController(findNavController())
             }
             usernameField.apply {
+                editText?.setText(args.username)
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     setAutofillHints(View.AUTOFILL_HINT_USERNAME)
                 }
             }
             passwordField.apply {
+                editText?.setText(args.password)
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     setAutofillHints(View.AUTOFILL_HINT_PASSWORD)
                 }
@@ -44,22 +43,20 @@ class SignInFragment : ParrotFragment<FragmentSignInBinding>() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        val username = binding?.usernameField?.editText?.text?.toString().orEmpty()
-        val password = binding?.passwordField?.editText?.text?.toString().orEmpty()
-        outState.apply {
-            putString(STATE_USERNAME_FIELD, username)
-            putString(STATE_PASSWORD_FIELD, password)
+        withBinding {
+            outState.apply {
+                putString(STATE_USERNAME_FIELD, usernameField.editText?.text?.toString().orEmpty())
+                putString(STATE_PASSWORD_FIELD, passwordField.editText?.text?.toString().orEmpty())
+            }
         }
     }
 
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
         super.onViewStateRestored(savedInstanceState)
         if (savedInstanceState == null) return
-        val username = savedInstanceState.getString(STATE_USERNAME_FIELD, "")
-        val password = savedInstanceState.getString(STATE_PASSWORD_FIELD, "")
         withBinding {
-            usernameField.editText?.setText(username)
-            passwordField.editText?.setText(password)
+            usernameField.editText?.setText(savedInstanceState.getString(STATE_USERNAME_FIELD, ""))
+            passwordField.editText?.setText(savedInstanceState.getString(STATE_PASSWORD_FIELD, ""))
         }
     }
 
