5fd16c37288518cd1914a73ffa09d89a60715cef
DanielFalcao <dev.danielfalcao@gmail.com>
2021-07-09 01:05:02 -0300

    Adding safe args lib

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,4 +12,5 @@ repositories {
 dependencies {
     implementation(kotlin("gradle-plugin", version = "1.5.20"))
     implementation("com.android.tools.build:gradle:4.2.2" )
+    implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
 }
